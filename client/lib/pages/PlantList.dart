d243b0887a79e70a6adf90ec4097544db682df81
Giovanni Zwaan <0971271@hr.nl>
2020-04-22 14:43:05 +0200

    plantlist v0.1

--- client/lib/pages/PlantList.dart
+++ client/lib/pages/PlantList.dart
@@ -12,18 +12,38 @@ class PlantList extends StatelessWidget {
         title: Text("Plant list", style: TextStyle(fontFamily: 'Libre Baskerville')),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Plant list',
-            ),
-          ],
-        ),
+      body: ListView.builder(
+          itemBuilder: (_, index) =>
+          PlantItem(
+              name: 'Garlic Boi',
+              image: 'assets/images/garlic_boi.png',
+              temp: index,
+          ),
+        itemCount: 15,
+      )
+    );
+  }
+}
+
+class PlantItem extends StatelessWidget {
+  final String name;
+  final String image;
+  final int temp;
+
+  PlantItem({this.name, this.image, this.temp});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      color: temp & 1 == 0 ? Colors.red : Colors.blue,
+      child: new Column(
+        children: <Widget>[
+          Text(name),
+          Image.asset(image,
+              height: 150.0, width: 150.0)
+        ],
       ),
     );
   }
-
-
 }
