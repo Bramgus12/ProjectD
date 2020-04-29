5dcfbdf8b3d403cb194c428a5523cf6c5c5b3f41
Giovanni Zwaan <0971271@hr.nl>
2020-04-29 12:35:34 +0200

    layout looks more like the design

--- client/lib/pages/PlantList.dart
+++ client/lib/pages/PlantList.dart
@@ -19,7 +19,6 @@ class PlantList extends StatelessWidget {
           PlantItem(
               name: plantNames[index % 5],
               image: 'assets/images/' + plantNames[index % 5] + '.jpg',
-              temp: index,
           ),
         itemCount: 25,
       )
@@ -30,35 +29,82 @@ class PlantList extends StatelessWidget {
 class PlantItem extends StatelessWidget {
   final String name;
   final String image;
-  final int temp;
 
-  PlantItem({this.name, this.image, this.temp});
+  final double _imageWidth = 150.0;
+  final double _imageHeight = 150.0;
+
+  PlantItem({this.name, this.image});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all()
+    return DefaultTextStyle(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              width: 1.0,
+              color: Colors.white
+            )
           ),
-          height: 200.0,
+          color: Colors.black
         ),
-        Positioned(
-          left: 225,
-          top: 50,
-          child: Text("Naam\n" + name)
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Image.asset(
+                image,
+                width: _imageWidth,
+                height: _imageHeight,
+              ),
+            ),
+            Positioned(
+              left: 200,
+              top: 0,
+              child: Text(
+                'Naam',
+                style: TextStyle(
+                  color: Colors.grey
+                ),
+              ),
+            ),
+            Positioned(
+              left: 200,
+              top: 20,
+              child: Text(
+                name,
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ),
+            Positioned(
+              left: 200,
+              top: 50,
+              child: Text(
+                'Hoevelheid zonlicht',
+                style: TextStyle(
+                    color: Colors.grey
+                ),
+              ),
+            ),
+            Positioned(
+              left: 200,
+              top: 100,
+              child: Text(
+                'Hoeveelheid water',
+                style: TextStyle(
+                    color: Colors.grey
+                ),
+              ),
+            )
+          ],
         ),
-        Positioned(
-          left: 15,
-          top: 25,
-          child: Image.asset(
-            image,
-            width: 150.0,
-            height: 150.0,
-          )
-        )
-      ],
+      ),
+      style: TextStyle(
+          fontFamily: 'Libre Baskerville'
+      ),
     );
   }
 }
