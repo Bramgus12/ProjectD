0fe68887f45a3e4812a97d15b24cfca5fd9ea6d9
Pedro Bronsveld <pedrobronsveld@gmail.com>
2020-05-11 10:10:33 +0200

    Added error checking for json fetch requests.

--- client/lib/api/ApiConnection.dart
+++ client/lib/api/ApiConnection.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:global_configuration/global_configuration.dart';
 import 'package:plantexpert/api/WeatherStation.dart';
@@ -11,17 +13,32 @@ class ApiConnection {
     : baseUrl = 'http://${GlobalConfiguration().getString("server")}:${GlobalConfiguration().getInt("port")}/api/';
 
   Future<dynamic> _fetchJson(String url) async {
-    http.Response response = await http.get(url);
-    if(response.statusCode != 200)
+    try {
+      http.Response response = await http.get(url);
+      if(response.statusCode != 200){
+        print("Received response with status code no equal to 200 while fetching url: $url");
+        print("Status Code: ${response.statusCode}");
+        return null;
+      }
+      return json.decode(response.body);
+    } on SocketException catch(e) {
+      print("SocketException occured while trying to fetch url: $url");
+      print(e);
       return null;
-    return json.decode(response.body);
+    } on FormatException catch(e) {
+      print("Invalid json received from url: $url");
+      print(e);
+      return null;
+    }
   }
   
   Future<List<WeatherStation>> fetchWeatherStations(String region) async {
-    Iterable jsonWeatherStations = await _fetchJson('${baseUrl}weather/$region');
-    if(jsonWeatherStations == null)
-      return null;
-    return jsonWeatherStations.map<WeatherStation>((jsonWeatherStation) => WeatherStation.fromJson(jsonWeatherStation)).toList();
+    dynamic jsonWeatherStations = await _fetchJson('${baseUrl}weather/$region');
+    if (jsonWeatherStations != null && jsonWeatherStations is Iterable)
+      return jsonWeatherStations.map<WeatherStation>((jsonWeatherStation) => WeatherStation.fromJson(jsonWeatherStation)).toList();
+    else
+      print("Error: jsonWeatherStations is not a list.");
+    return null;
   }
 
 }
