7ed2d732518653ca507fb15adc16fc472a031735
Albert <0961988@hr.nl>
2020-05-14 15:56:48 +0200

    added rough layout of first time use camera

--- client/lib/pages/Camera.dart
+++ client/lib/pages/Camera.dart
@@ -8,41 +8,43 @@ import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img;
+import 'package:localstorage/localstorage.dart';
+
 class Camera extends StatefulWidget {
   @override
   _CameraState createState() => _CameraState();
 }
 
 class _CameraState extends State<Camera> {
-
   File imageFile;
   List _recognitions;
   bool _busy = false;
+  int activeCameraItem = 0;
 
-  openGallery( BuildContext context) async {
+  openGallery(BuildContext context) async {
     this.setState(() {
       _recognitions = null;
     });
     var img = await ImagePicker.pickImage(source: ImageSource.gallery);
     predictImage(img);
-    this.setState((){
+    this.setState(() {
       imageFile = img;
     });
     Navigator.of(context).pop();
-
   }
-  openCamera(BuildContext context) async{
+
+  openCamera(BuildContext context) async {
     this.setState(() {
       _recognitions = null;
     });
     var img = await ImagePicker.pickImage(source: ImageSource.camera);
     predictImage(img);
-    this.setState((){
+    this.setState(() {
       imageFile = img;
-
     });
     Navigator.of(context).pop();
   }
+
   Future<void> imageSourceChoiceDialog(BuildContext context) {
     return showDialog<void>(
         context: context,
@@ -54,14 +56,16 @@ class _CameraState extends State<Camera> {
                 children: <Widget>[
                   GestureDetector(
                     child: Text("Gallary"),
-                    onTap: (){
+                    onTap: () {
                       openGallery(context);
                     },
                   ),
-                  Padding(padding: EdgeInsets.all(8.0),),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
                   GestureDetector(
                     child: Text("Camera"),
-                    onTap: (){
+                    onTap: () {
                       openCamera(context);
                     },
                   ),
@@ -69,25 +73,32 @@ class _CameraState extends State<Camera> {
               ),
             ),
           );
-        }
-    );
+        });
   }
 
-
-  Widget selectedImageView(){
-    if(imageFile == null){
+  Widget selectedImageView() {
+    if (imageFile == null) {
       return Text("No image Selected!", textAlign: TextAlign.center);
-
-    }else{
-      return  Image.file(imageFile,width: 400, height: 400,);
+    } else {
+      return Image.file(
+        imageFile,
+        width: 400,
+        height: 400,
+      );
     }
   }
-  Widget predictionView(){
-    if(_recognitions == null){
+
+  Widget predictionView() {
+    if (_recognitions == null) {
       return Text("", textAlign: TextAlign.center);
-    }else{
+    } else {
       return Text(
-          _recognitions.map((res){ return "Prediction: ${res["label"]}: Confidence: ${res["confidence"].toStringAsFixed(3)}";}).toList()[0].toString(),
+        _recognitions
+            .map((res) {
+              return "Prediction: ${res["label"]}: Confidence: ${res["confidence"].toStringAsFixed(3)}";
+            })
+            .toList()[0]
+            .toString(),
         textAlign: TextAlign.center,
         style: TextStyle(
           color: Colors.black,
@@ -95,23 +106,198 @@ class _CameraState extends State<Camera> {
           background: Paint()..color = Colors.white,
         ),
       );
-
+    }
   }
-  }
 
+  Widget cameraUsage() {
+    return AlertDialog(
+        title: Text("Camera gebruik"),
+        content: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                      child: activeCameraItem == 0
+                          ? Image.asset(
+                              "assets/images/camera/usage-of-camera.jpg",
+                              height: 250,
+                              fit: BoxFit.scaleDown,
+                            )
+                          : activeCameraItem == 1
+                              ? Image.asset(
+                                  "assets/images/camera/probable-plants.jpg",
+                                  height: 250,
+                                  fit: BoxFit.scaleDown,
+                                )
+                              : activeCameraItem == 2
+                                  ? Image.asset(
+                                      "assets/images/camera/add-plant.jpg",
+                                      height: 250,
+                                      fit: BoxFit.scaleDown,
+                                    )
+                                  : activeCameraItem == 3
+                                      ? Image.asset(
+                                          "assets/images/camera/custom-plant.jpg",
+                                          height: 250,
+                                          fit: BoxFit.scaleDown,
+                                        )
+                                      : Image.asset(
+                                          "assets/images/camera/swipe-to-hide.jpg",
+                                          height: 250,
+                                          fit: BoxFit.scaleDown,
+                                        )),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                      child: activeCameraItem == 0
+                          ? Text(
+                              "0: Text describing the usage of the camera in the application",
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 5,
+                            )
+                          : activeCameraItem == 1
+                              ? Text(
+                                  "1: Text describing the list of suggested plants",
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 5,
+                                )
+                              : activeCameraItem == 2
+                                  ? Text(
+                                      "2: Text describing the details of the plant",
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 5,
+                                    )
+                                  : activeCameraItem == 3
+                                      ? Text(
+                                          "3: Text describing the adding of user data to the plant",
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 5,
+                                        )
+                                      : Text(
+                                          "ELSE: Text describing the user can swipe left or right to hide this message",
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 5,
+                                        )),
+                ],
+              ),
+            ),
+
+            //Dotted lines
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InkWell(
+                  onTap: () => {
+                    setState(() {
+                      activeCameraItem = 0;
+                    })
+                  },
+                  child: new Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: activeCameraItem == 0
+                          ? Colors.grey[850]
+                          : Colors.grey[600],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => {
+                    setState(() {
+                      activeCameraItem = 1;
+                    })
+                  },
+                  child: new Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: activeCameraItem == 1
+                          ? Colors.grey[850]
+                          : Colors.grey[600],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => {
+                    setState(() {
+                      activeCameraItem = 2;
+                    })
+                  },
+                  child: new Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: activeCameraItem == 2
+                          ? Colors.grey[850]
+                          : Colors.grey[600],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => {
+                    setState(() {
+                      activeCameraItem = 3;
+                    })
+                  },
+                  child: new Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: activeCameraItem == 3
+                          ? Colors.grey[850]
+                          : Colors.grey[600],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => {
+                    setState(() {
+                      activeCameraItem = 4;
+                    })
+                  },
+                  child: new Container(
+                    margin: const EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: activeCameraItem == 4
+                          ? Colors.grey[850]
+                          : Colors.grey[600],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
 
   Future loadModel() async {
     Tflite.close();
     try {
       String res;
       res = await Tflite.loadModel(
-          model: "assets/m0_0.81.tflite",
-          labels: "assets/label.txt");
+          model: "assets/m0_0.81.tflite", labels: "assets/label.txt");
       print(res);
     } on PlatformException {
       print('Failed to load model.');
     }
   }
+
   Future plantModel(File image) async {
     var recognitions = await Tflite.runModelOnImage(
       path: image.path,
@@ -123,13 +309,11 @@ class _CameraState extends State<Camera> {
     print("----------------------------------------------------");
     print(recognitions);
     setState(() {
-        _recognitions = recognitions;
+      _recognitions = recognitions;
     });
   }
 
   Future plantModelWithBinary(img.Image image) async {
-
-
     Uint8List imageToByteListFloat32(
         img.Image image, int inputSize, double mean, double std) {
       var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
@@ -146,25 +330,24 @@ class _CameraState extends State<Camera> {
 
       return convertedBytes.buffer.asUint8List();
     }
+
     var recognitions = await Tflite.runModelOnBinary(
-        binary: imageToByteListFloat32(image, 200, 0.4526901778594428, 0.3290300460265408),// required
-        numResults: 6,    // defaults to 5
-        threshold: 0.05,  // defaults to 0.1
-        asynch: true      // defaults to true
-    );
+        binary: imageToByteListFloat32(
+            image, 200, 0.4526901778594428, 0.3290300460265408), // required
+        numResults: 6, // defaults to 5
+        threshold: 0.05, // defaults to 0.1
+        asynch: true // defaults to true
+        );
     print("----------------------------------------------------");
     print(recognitions);
     setState(() {
-        _recognitions = recognitions;
+      _recognitions = recognitions;
     });
   }
 
-
   Future predictImage(File image) async {
-
     await plantModel(image);
-    }
-
+  }
 
   @override
   void initState() {
@@ -182,50 +365,45 @@ class _CameraState extends State<Camera> {
   @override
   Widget build(BuildContext context) {
     List<Widget> stackChildren = [];
+    final LocalStorage storage = new LocalStorage('by_living_art');
 
-    if (_busy) {
-      stackChildren.add(const Opacity(
-        child: ModalBarrier(dismissible: false, color: Colors.grey),
-        opacity: 0.3,
+    stackChildren.addAll([selectedImageView(), predictionView()]);
+
+    if(storage.getItem("first_time_usage") == null){
+      stackChildren.add(Dismissible(
+        key: UniqueKey(),
+        child: cameraUsage(),
       ));
-      stackChildren.add(const Center(child: CircularProgressIndicator()));
+
+      storage.setItem("first_time_usage", true);
     }
-
-
-
-
 
     return Scaffold(
       drawer: MenuNavigation(),
       bottomNavigationBar: BottomNavigation(),
       appBar: AppBar(
-        title: Text("Camera", style: TextStyle(fontFamily: 'Libre Baskerville')),
+        title:
+            Text("Camera", style: TextStyle(fontFamily: 'Libre Baskerville')),
         centerTitle: true,
       ),
-      body:
-          Container(
-
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                selectedImageView(),
-                predictionView()
-              ],
-            ),
-
-          ),
-
+      body: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: stackChildren,
+        ),
+      ),
 
 //
-    floatingActionButton:FloatingActionButton(
-      onPressed: (){imageSourceChoiceDialog(context);},
-      child: Icon(Icons.image, color: Colors.white,),
-      backgroundColor: Colors.blueAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => imageSourceChoiceDialog(context),
+        child: Icon(
+          Icons.image,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.blueAccent,
       ),
     );
   }
 }
-
-
