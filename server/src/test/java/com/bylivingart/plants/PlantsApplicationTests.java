3ab95ceb5413fb505d795585193c4f10286c4540
Albert <0961988@hr.nl>
2020-05-31 22:41:25 +0200

    Added testability to running as application

--- server/src/test/java/com/bylivingart/plants/PlantsApplicationTests.java
+++ server/src/test/java/com/bylivingart/plants/PlantsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.bylivingart.plants;
 
+import org.junit.internal.runners.statements.Fail;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PlantsApplicationTests {
+//@SpringBootTest
+public class PlantsApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String args[]) {
+		Result res = JUnitCore.runClasses(PlantsApplicationTestsRunner.class);
+		for(Failure fail : res.getFailures()){
+			System.out.printf(fail.toString());
+		}
+
+		System.out.printf("Test completed, result: %s, failed: %s\n", res.wasSuccessful() ? "success" : "failed", res.getFailureCount());
 	}
-
 }
---
+++ server/src/test/java/com/bylivingart/plants/PlantsApplicationTestsRunner.java
@@ -0,0 +1,14 @@
+package com.bylivingart.plants;
+
+import com.bylivingart.plants.controllers.WeatherControllerTests;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({WeatherControllerTests.class})
+public class PlantsApplicationTestsRunner {
+    @Test
+    void contextLoads() {
+    }
+}
